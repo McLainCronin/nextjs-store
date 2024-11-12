70ffc3e7995faff1f2448fe2b8b51f95f16fc7f7
McLainCronin <cronin97@gmail.com>
2024-11-11 17:35:40 -0800

    Add Image import to ProductCard, fix Link href in CancelPage, and update next.config for SWC minification

--- app/ProductCard.js
+++ app/ProductCard.js
@@ -3,6 +3,7 @@ import React from 'react'
 import {useRouter} from 'next/navigation'
 import useCart from './(store)/store'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function ProductCard(props) {
   const { product } = props
--- app/cancel/page.js
+++ app/cancel/page.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function CancelPage() {
     return (
         <div>
-            <Link>Return Home</Link>
+            <Link href={'/'}>Return Home</Link>
         </div>
     )
 }
